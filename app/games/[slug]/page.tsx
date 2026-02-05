2a5bec76905d6087277936b6a289cdc069408825
RachaponKjr <rachapon.navy@gmail.com>
2026-02-05 17:38:41 +0700

    fix

--- app/games/[slug]/page.tsx
+++ app/games/[slug]/page.tsx
@@ -1,4 +1,14 @@
 import TopUpPage from "./_components/topup";
+export async function generateStaticParams() {
+  // หากคุณมีรายการ slug ในมือ หรือดึงมาจาก API/Database
+  // ให้คืนค่ากลับไปเป็น array ของ object แบบนี้ครับ
+  const games = [{ slug: "game-1" }, { slug: "game-2" }];
+
+  return games.map((game) => ({
+    slug: game.slug,
+  }));
+}
+
 const page = () => {
   return <TopUpPage />;
 };
