18f6e46cd701ceae8c9aab0f0f1ef289a1b7a035
RachaponKjr <rachapon.navy@gmail.com>
2026-02-07 14:31:59 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomePage from "./_components/home-page";
 import { BannerType } from "@/types/banner.type";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/banner");
-  const data = (await res.json()) as BannerType[];
-  return <HomePage banner={data} />;
+  // const res = await fetch("http://localhost:3000/banner");
+  // const data = (await res.json()) as BannerType[];
+  return <HomePage banner={[]} />;
 }
