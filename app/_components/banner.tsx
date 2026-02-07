029adf0891d9499960c63a15876bb4fc1b690ea3
RachaponKjr <rachapon.navy@gmail.com>
2026-02-07 14:34:18 +0700

    update

--- app/_components/banner.tsx
+++ app/_components/banner.tsx
@@ -8,18 +8,18 @@ const Banner = ({ banner }: { banner: BannerType[] }) => {
     <div className="relative my-10 md:my-20 max-w-11/13 md:max-w-10/12 mx-auto">
       <div className="bg-neutral-200 w-full aspect-16/8 flex items-end relative">
         <Image
-          src={banner[0].imageUrl}
-          alt={banner[0].title}
+          src={banner[0]?.imageUrl || ""}
+          alt={banner[0]?.title || ""}
           fill
           className="object-cover"
         />
         <div className="absolute top-0 left-0 w-full h-full bg-linear-to-t from-black/50 via-black/10 to-transparent z-20" />
         <div className="text-white flex flex-col md:gap-2 p-4 md:p-10 z-50">
           <h5 className="text-sm md:text-4xl max-w-3xl font-bold line-clamp-1">
-            {banner[0].title}
+            {banner[0]?.title || ""}
           </h5>
           <p className="max-w-2xl text-xs md:text-lg line-clamp-2">
-            {banner[0].description}
+            {banner[0]?.description || ""}
           </p>
         </div>
       </div>
